1d8c75f9795870fdf53c969966943ccac02955c1
Tharun-Reddy21 <138970712+Tharun-Reddy21@users.noreply.github.com>
2024-01-05 23:21:07 +0530

    Update ajax.php

--- functions/ajax.php
+++ functions/ajax.php
@@ -19,43 +19,5 @@ while($rows =mysqli_fetch_assoc($result)) {
 
 
    fclose($fp);
-//
-// while($row = mysqli_fetch_array($result)){
-// // $array=$row->fetch_assoc();
-//
-// //Now encode PHP array in JSON string
-// $json=json_encode($row,true);
-//
-// //test the json string
-// var_dump($json);
-//
-// //create file if not exists
-// $fo=fopen("C:/xampp/htdocs/login/functions/myjson.json","w");
-//
-// //write the json string in file
-// $fr=fwrite($fo,$json);
-//
-// }
-}
 
-// function storejson(){
-// //select all data from json table
-// $query="select * from users ";
-// $result=query($query);
-//
-// //fetech all data from json table in associative array format and store in $result variable
-// $array=fetch_array($result);
-//
-// //Now encode PHP array in JSON string
-// $json=json_encode($array,true);
-//
-// //test the json string
-// //var_dump($json);
-//
-// //create file if not exists
-// $fo=fopen("myjson.json","w");
-//
-// //write the json string in file
-// $fr=fwrite($fo,$json);
-// }
 ?>
